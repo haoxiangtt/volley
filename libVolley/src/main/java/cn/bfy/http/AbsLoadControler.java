aa0f1042c0cb61f4ccad7526b5f96ec6558e7a87
ouyangjinfu <ouyangjinfu@leagsoft.com>
2019-02-22 17:22:23 +0800

    change

---
+++ libVolley/src/main/java/cn/bfy/http/AbsLoadControler.java
@@ -0,0 +1,36 @@
+package cn.bfy.http;
+
+import cn.bfy.http.base.LoadControler;
+import cn.bfy.volley.Request;
+
+/**
+ * <pre>
+ * copyright  : Copyright ©2004-2018 版权所有　XXXXXXXXXXXXXXXXX
+ * company    : XXXXXXXXXXXXXXXXX
+ * @author     : OuyangJinfu
+ * e-mail     : jinfu123.-@163.com
+ * createDate : 2017/4/13 0023
+ * modifyDate : 2017/4/13 0023
+ * @version    : 1.0
+ * desc       : Abstract LoaderControler that implements LoadControler
+ * </pre>
+ */
+public abstract class AbsLoadControler implements LoadControler {
+
+    protected Request<?> mRequest;
+
+    public void bindRequest(Request<?> request) {
+        this.mRequest = request;
+    }
+
+    @Override
+    public void cancel() {
+        if (this.mRequest != null) {
+            this.mRequest.cancel();
+        }
+    }
+
+    protected String getUrl() {
+        return this.mRequest.getUrl();
+    }
+}
